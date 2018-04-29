73f96d5c61fee6f309a75e0ca5f2eaf74b05d6fc
Saurabh Bayani <saurabh.bayani@gmail.com>
2018-04-29 18:24:20 +0530

    Adding webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+var config = {
+    watch: true, // turn false in production
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        inline: true,
+        port: 8080,
+        publicPath: "/",
+        contentBase: "./dist",
+        hot: true
+    },
+    // mode: 'production',  // Uncomment in production
+    mode: 'development',  // Comment in production
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
+module.exports = config;
