80eabdfb743ed1a07eade2261dd9fd206e18f5bc
Saurabh Bayani <saurabh.bayani@gmail.com>
2018-04-29 19:11:56 +0530

    Adding Folders

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import App from '../src/Component/App.jsx';
+
+ReactDOM.render(
+    <App />
+    , document.querySelector('#root'));
